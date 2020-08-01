1fa190d39b86bb2cddf456e60909de9b89f2ddb3
Patrik Nusszer <patrik.nusszer@icloud.com>
2020-08-01 20:41:17 +0200

    multilingual 0.75

--- Controllers/SoftwaresController.cs
+++ Controllers/SoftwaresController.cs
@@ -12,157 +12,182 @@ namespace Nusstudios.Controllers
 {
     public class SoftwaresController : Controller
     {
-        [Route("{gb?}")]
+        [Route("{controller}/{action}/{gb?}")]
         public IActionResult CPPO(bool? gb)
         {
             string val;
             return View(new SoftwaresViewModel("https://nusstudios.com/Softwares/CPPO", true, false, gb != null ? (bool)gb : ((val = Tools.GetCookieValue(Request, "gb")) == null ? false : bool.Parse(val))));
         }
 
+        [Route("{controller}/{action}/{gb?}")]
         public IActionResult nuss(bool? gb)
         {
             string val;
             return View(new SoftwaresViewModel("https://nusstudios.com/Softwares/nuss", true, false, gb != null ? (bool)gb : ((val = Tools.GetCookieValue(Request, "gb")) == null ? false : bool.Parse(val))));
         }
 
+        [Route("{controller}/{action}/{gb?}")]
         public IActionResult nussdesktop(bool? gb)
         {
             string val;
             return View(new SoftwaresViewModel("https://nusstudios.com/Softwares/nussdesktop", true, false, gb != null ? (bool)gb : ((val = Tools.GetCookieValue(Request, "gb")) == null ? false : bool.Parse(val))));
         }
 
-		public IActionResult nussdesktop_javafx(bool? gb)
+        [Route("{controller}/{action}/{gb?}")]
+        public IActionResult nussdesktop_javafx(bool? gb)
 		{
             string val;
             return View(new SoftwaresViewModel("https://nusstudios.com/Softwares/nussdesktop_javafx", true, false, gb != null ? (bool)gb : ((val = Tools.GetCookieValue(Request, "gb")) == null ? false : bool.Parse(val))));
         }
 
-		public IActionResult swifflib(bool? gb)
+        [Route("{controller}/{action}/{gb?}")]
+        public IActionResult swifflib(bool? gb)
         {
             string val;
             return View(new SoftwaresViewModel("https://nusstudios.com/Softwares/swifflib", true, true, gb != null ? (bool)gb : ((val = Tools.GetCookieValue(Request, "gb")) == null ? false : bool.Parse(val))));
         }
 
+        [Route("{controller}/{action}/{gb?}")]
         public IActionResult equslvr(bool? gb)
         {
             string val;
             return View(new SoftwaresViewModel("https://nusstudios.com/Softwares/equslvr", true, false, gb != null ? (bool)gb : ((val = Tools.GetCookieValue(Request, "gb")) == null ? false : bool.Parse(val))));
         }
 
+        [Route("{controller}/{action}/{gb?}")]
         public IActionResult cupmixer(bool? gb)
         {
             string val;
             return View(new SoftwaresViewModel("https://nusstudios.com/Softwares/cupmixer", false, false, gb != null ? (bool)gb : ((val = Tools.GetCookieValue(Request, "gb")) == null ? false : bool.Parse(val))));
         }
 
+        [Route("{controller}/{action}/{gb?}")]
         public IActionResult gia(bool? gb)
         {
             string val;
             return View(new SoftwaresViewModel("https://nusstudios.com/Softwares/gia", true, false, gb != null ? (bool)gb : ((val = Tools.GetCookieValue(Request, "gb")) == null ? false : bool.Parse(val))));
         }
 
+        [Route("{controller}/{action}/{gb?}")]
         public IActionResult nussattribute(bool? gb)
         {
             string val;
             return View(new SoftwaresViewModel("https://nusstudios.com/Softwares/nussattribute", true, false, gb != null ? (bool)gb : ((val = Tools.GetCookieValue(Request, "gb")) == null ? false : bool.Parse(val))));
         }
 
+        [Route("{controller}/{action}/{gb?}")]
         public IActionResult stringmath(bool? gb)
 		{
             string val;
             return View(new SoftwaresViewModel("https://nusstudios.com/Softwares/stringmath", true, false, gb != null ? (bool)gb : ((val = Tools.GetCookieValue(Request, "gb")) == null ? false : bool.Parse(val))));
         }
 
-		public IActionResult stringmath_python(bool? gb)
+        [Route("{controller}/{action}/{gb?}")]
+        public IActionResult stringmath_python(bool? gb)
 		{
             string val;
             return View(new SoftwaresViewModel("https://nusstudios.com/Softwares/stringmath_python", false, false, gb != null ? (bool)gb : ((val = Tools.GetCookieValue(Request, "gb")) == null ? false : bool.Parse(val))));
         }
 
-		public IActionResult jcore(bool? gb)
+        [Route("{controller}/{action}/{gb?}")]
+        public IActionResult jcore(bool? gb)
 		{
             string val;
             return View(new SoftwaresViewModel("https://nusstudios.com/Softwares/jcore", false, false, gb != null ? (bool)gb : ((val = Tools.GetCookieValue(Request, "gb")) == null ? false : bool.Parse(val))));
         }
 
+        [Route("{controller}/{action}/{gb?}")]
         public IActionResult nusstudioscorecsharp(bool? gb)
         {
             string val;
             return View(new SoftwaresViewModel("https://nusstudios.com/Softwares/nusstudioscorecsharp", true, false, gb != null ? (bool)gb : ((val = Tools.GetCookieValue(Request, "gb")) == null ? false : bool.Parse(val))));
         }
 
+        [Route("{controller}/{action}/{gb?}")]
         public IActionResult imgtopdfui(bool? gb)
 		{
             string val;
             return View(new SoftwaresViewModel("https://nusstudios.com/Softwares/imgtopdfui", true, false, gb != null ? (bool)gb : ((val = Tools.GetCookieValue(Request, "gb")) == null ? false : bool.Parse(val))));
         }
 
-		public IActionResult imgtopdf(bool? gb)
+        [Route("{controller}/{action}/{gb?}")]
+        public IActionResult imgtopdf(bool? gb)
 		{
             string val;
             return View(new SoftwaresViewModel("https://nusstudios.com/Softwares/imgtopdf", false, false, gb != null ? (bool)gb : ((val = Tools.GetCookieValue(Request, "gb")) == null ? false : bool.Parse(val))));
         }
 
-		public IActionResult spltoemf(bool? gb)
+        [Route("{controller}/{action}/{gb?}")]
+        public IActionResult spltoemf(bool? gb)
 		{
             string val;
             return View(new SoftwaresViewModel("https://nusstudios.com/Softwares/spltoemf", true, false, gb != null ? (bool)gb : ((val = Tools.GetCookieValue(Request, "gb")) == null ? false : bool.Parse(val))));
         }
 
+        [Route("{controller}/{action}/{gb?}")]
         public IActionResult nusstumblr(bool? gb)
         {
             string val;
             return View(new SoftwaresViewModel("https://nusstudios.com/Softwares/nusstumblr", true, false, gb != null ? (bool)gb : ((val = Tools.GetCookieValue(Request, "gb")) == null ? false : bool.Parse(val))));
         }
 
-		public IActionResult instarchiver(bool? gb)
+        [Route("{controller}/{action}/{gb?}")]
+        public IActionResult instarchiver(bool? gb)
 		{
             string val;
             return View(new SoftwaresViewModel("https://nusstudios.com/Softwares/instarchiver", true, false, gb != null ? (bool)gb : ((val = Tools.GetCookieValue(Request, "gb")) == null ? false : bool.Parse(val))));
         }
 
+        [Route("{controller}/{action}/{gb?}")]
         public IActionResult pixie(bool? gb)
         {
             string val;
             return View(new SoftwaresViewModel("https://nusstudios.com/Softwares/pixie", true, false, gb != null ? (bool)gb : ((val = Tools.GetCookieValue(Request, "gb")) == null ? false : bool.Parse(val))));
         }
 
+        [Route("{controller}/{action}/{gb?}")]
         public IActionResult pixie_macos(bool? gb)
         {
             string val;
             return View(new SoftwaresViewModel("https://nusstudios.com/Softwares/pixie_macos", true, false, gb != null ? (bool)gb : ((val = Tools.GetCookieValue(Request, "gb")) == null ? false : bool.Parse(val))));
         }
 
+        [Route("{controller}/{action}/{gb?}")]
         public IActionResult tfutilpp(bool? gb)
         {
             string val;
             return View(new SoftwaresViewModel("https://nusstudios.com/Softwares/tfutilpp", true, false, gb != null ? (bool)gb : ((val = Tools.GetCookieValue(Request, "gb")) == null ? false : bool.Parse(val))));
         }
 
+        [Route("{controller}/{action}/{gb?}")]
         public IActionResult keylogger(bool? gb)
         {
             string val;
             return View(new SoftwaresViewModel("https://nusstudios.com/Softwares/keylogger", true, false, gb != null ? (bool)gb : ((val = Tools.GetCookieValue(Request, "gb")) == null ? false : bool.Parse(val))));
         }
 
+        [Route("{controller}/{action}/{gb?}")]
         public IActionResult xorcrypt(bool? gb)
         {
             string val;
             return View(new SoftwaresViewModel("https://nusstudios.com/Softwares/xorcrypt", false, false, gb != null ? (bool)gb : ((val = Tools.GetCookieValue(Request, "gb")) == null ? false : bool.Parse(val))));
         }
 
+        [Route("{controller}/{action}/{gb?}")]
         public IActionResult lilcryptoc(bool? gb)
         {
             string val;
             return View(new SoftwaresViewModel("https://nusstudios.com/Softwares/lilcryptoc", true, false, gb != null ? (bool)gb : ((val = Tools.GetCookieValue(Request, "gb")) == null ? false : bool.Parse(val))));
         }
 
+        [Route("{controller}/{action}/{gb?}")]
         public IActionResult lilcryptocpp(bool? gb)
         {
             string val;
             return View(new SoftwaresViewModel("https://nusstudios.com/Softwares/lilcryptocpp", true, false, gb != null ? (bool)gb : ((val = Tools.GetCookieValue(Request, "gb")) == null ? false : bool.Parse(val))));
         }
 
+        [Route("{controller}/{action}/{gb?}")]
         public IActionResult torrentchk(bool? gb)
         {
             string val;
