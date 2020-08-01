7aafff7337d97f51d4684c6d980df95d677cfd3d
Patrik Nusszer <patrik.nusszer@icloud.com>
2020-08-01 22:04:14 +0200

    multilingual 0.99

--- Controllers/ArticlesController.cs
+++ Controllers/ArticlesController.cs
@@ -15,35 +15,35 @@ namespace Nusstudios.Controllers
         public IActionResult dijkstra(bool? gb)
         {
             string val;
-            return View(new ArticlesViewModel("https://nusstudios.com/Blog/dijkstra", false, false, gb != null ? (bool)gb : ((val = Tools.GetCookieValue(Request, "gb")) == null ? false : bool.Parse(val))));
+            return View(new ArticlesViewModel("https://nusstudios.com/Articles/dijkstra", false, false, gb != null ? (bool)gb : ((val = Tools.GetCookieValue(Request, "gb")) == null ? false : bool.Parse(val))));
         }
 
         [Route("{controller}/{action}/{gb?}")]
         public IActionResult heapsort(bool? gb)
         {
             string val;
-            return View(new ArticlesViewModel("https://nusstudios.com/Blog/heapsort", false, false, gb != null ? (bool)gb : ((val = Tools.GetCookieValue(Request, "gb")) == null ? false : bool.Parse(val))));
+            return View(new ArticlesViewModel("https://nusstudios.com/Articles/heapsort", false, false, gb != null ? (bool)gb : ((val = Tools.GetCookieValue(Request, "gb")) == null ? false : bool.Parse(val))));
         }
 
         [Route("{controller}/{action}/{gb?}")]
         public IActionResult mergesort(bool? gb)
         {
             string val;
-            return View(new ArticlesViewModel("https://nusstudios.com/Blog/mergesort", false, false, gb != null ? (bool)gb : ((val = Tools.GetCookieValue(Request, "gb")) == null ? false : bool.Parse(val))));
+            return View(new ArticlesViewModel("https://nusstudios.com/Articles/mergesort", false, false, gb != null ? (bool)gb : ((val = Tools.GetCookieValue(Request, "gb")) == null ? false : bool.Parse(val))));
         }
 
         [Route("{controller}/{action}/{gb?}")]
         public IActionResult knuthmorrispratt(bool? gb)
         {
             string val;
-            return View(new ArticlesViewModel("https://nusstudios.com/Blog/knuthmorrispratt", false, false, gb != null ? (bool)gb : ((val = Tools.GetCookieValue(Request, "gb")) == null ? false : bool.Parse(val))));
+            return View(new ArticlesViewModel("https://nusstudios.com/Articles/knuthmorrispratt", false, false, gb != null ? (bool)gb : ((val = Tools.GetCookieValue(Request, "gb")) == null ? false : bool.Parse(val))));
         }
 
         [Route("{controller}/{action}/{gb?}")]
         public IActionResult floydstortoiseandhare(bool? gb)
         {
             string val;
-            return View(new ArticlesViewModel("https://nusstudios.com/Blog/floydstortoiseandhare", false, false, gb != null ? (bool)gb : ((val = Tools.GetCookieValue(Request, "gb")) == null ? false : bool.Parse(val))));
+            return View(new ArticlesViewModel("https://nusstudios.com/Articles/floydstortoiseandhare", false, false, gb != null ? (bool)gb : ((val = Tools.GetCookieValue(Request, "gb")) == null ? false : bool.Parse(val))));
         }
 
         // GET: /<controller>/
@@ -51,35 +51,35 @@ namespace Nusstudios.Controllers
         public IActionResult transformers_game_configurations_low_level(bool? gb)
         {
             string val;
-            return View(new ArticlesViewModel("https://nusstudios.com/Blog/transformers_game_configurations_low_level", false, false, gb != null ? (bool)gb : ((val = Tools.GetCookieValue(Request, "gb")) == null ? false : bool.Parse(val))));
+            return View(new ArticlesViewModel("https://nusstudios.com/Articles/transformers_game_configurations_low_level", false, false, gb != null ? (bool)gb : ((val = Tools.GetCookieValue(Request, "gb")) == null ? false : bool.Parse(val))));
         }
 
         [Route("{controller}/{action}/{gb?}")]
         public IActionResult youtube_ciphered_signatures(bool? gb)
         {
             string val;
-            return View(new ArticlesViewModel("https://nusstudios.com/Blog/youtube_ciphered_signatures", false, false, gb != null ? (bool)gb : ((val = Tools.GetCookieValue(Request, "gb")) == null ? false : bool.Parse(val))));
+            return View(new ArticlesViewModel("https://nusstudios.com/Articles/youtube_ciphered_signatures", false, false, gb != null ? (bool)gb : ((val = Tools.GetCookieValue(Request, "gb")) == null ? false : bool.Parse(val))));
         }
 
         [Route("{controller}/{action}/{gb?}")]
         public IActionResult graph_api_and_x_instagram_gis(bool? gb)
         {
             string val;
-            return View(new ArticlesViewModel("https://nusstudios.com/Blog/graph_api_and_x_instagram_gis", false, false, gb != null ? (bool)gb : ((val = Tools.GetCookieValue(Request, "gb")) == null ? false : bool.Parse(val))));
+            return View(new ArticlesViewModel("https://nusstudios.com/Articles/graph_api_and_x_instagram_gis", false, false, gb != null ? (bool)gb : ((val = Tools.GetCookieValue(Request, "gb")) == null ? false : bool.Parse(val))));
         }
 
         [Route("{controller}/{action}/{gb?}")]
         public IActionResult paradigms_brief_history(bool? gb)
         {
             string val;
-            return View(new ArticlesViewModel("https://nusstudios.com/Blog/paradigms_brief_history", false, false, gb != null ? (bool)gb : ((val = Tools.GetCookieValue(Request, "gb")) == null ? false : bool.Parse(val))));
+            return View(new ArticlesViewModel("https://nusstudios.com/Articles/paradigms_brief_history", false, false, gb != null ? (bool)gb : ((val = Tools.GetCookieValue(Request, "gb")) == null ? false : bool.Parse(val))));
         }
 
         [Route("{controller}/{action}/{gb?}")]
         public IActionResult high_level_languages_behind_the_scenes(bool? gb)
         {
             string val;
-            return View(new ArticlesViewModel("https://nusstudios.com/Blog/high_level_languages_behind_the_scenes", false, false, gb != null ? (bool)gb : ((val = Tools.GetCookieValue(Request, "gb")) == null ? false : bool.Parse(val))));
+            return View(new ArticlesViewModel("https://nusstudios.com/Articles/high_level_languages_behind_the_scenes", false, false, gb != null ? (bool)gb : ((val = Tools.GetCookieValue(Request, "gb")) == null ? false : bool.Parse(val))));
         }
     }
 }
